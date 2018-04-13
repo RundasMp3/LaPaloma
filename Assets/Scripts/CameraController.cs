0f18b71309a1ff0faf4f6d9a5671bdf2cb63107d
RundasMp3 <jorge.mendez@cetys.edu.mx>
2018-04-12 17:51:10 -0700

    Movement update

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float turnSpeed = 4.0f;
+    public Transform player;
+    public float height = 1f;
+    public float distance = 2f;
+
+    private Vector3 offsetX;
+    private Vector3 offsetY;
+
+    void Start()
+    {
+
+        offsetX = new Vector3(0, height, distance);
+        offsetY = new Vector3(0, 0, distance);
+    }
+
+    void LateUpdate()
+    {
+        offsetX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offsetX;
+        offsetY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.right) * offsetY;
+        transform.position = player.position + offsetX + offsetY;
+        transform.LookAt(player.position);
+    }
+}
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private CharacterController controller;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    private float verticalVelocity;
+    public float gravity = 14.0f;
+    private float jumpForce = 6.0f;
+    private float flightSpeed = 12.0f;
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+    private float playerSpeed = 5.0f;
+    private float rotateSpeed = 5.0f;
+    public float fall = 1.0f;
+    public float currYRot;
+    private float v;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveVector = Vector3.zero;
+        RaycastHit hit;
+        Ray vuelo = new Ray(transform.position, Vector3.down);
+
+        Debug.DrawRay(transform.position, Vector3.down * 2);
+
+        float h;
+
+
+        v = 0;
+        h = horizontalSpeed * Input.GetAxis("Mouse X");
+        fall = 2f;
+        if (transform.rotation.z != 0 && transform.rotation.x != 0 && controller.isGrounded)
+        {
+            currYRot = transform.eulerAngles.y;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, currYRot, 0), 2f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            verticalVelocity = jumpForce;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * playerSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.forward * Time.deltaTime * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotateSpeed, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotateSpeed, 0);
+        }
+
+        verticalVelocity -= gravity * Time.deltaTime * fall;
+
+
+
+
+
+        moveVector.y = verticalVelocity;
+        controller.Move(moveVector * Time.deltaTime);
+    }
+}
