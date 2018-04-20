5d74fdf24c80b7da5a2026b0f6d3ee164b79c7e4
Orlando Estrella L <oestrellal96@gmail.com>
2018-04-19 17:30:32 -0700

    Tried to fix everything

---
+++ Assets/Scripts/TutorialMessages/TutorialMessageTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMessageTrigger : MonoBehaviour {
+
+    /// <summary>
+    /// The instruction to be displayed.
+    /// </summary>
+    [TextArea(1, 3)]
+    public string instruction;
+
+    private bool hasBeenShown = false;
+
+    public void TriggerDialogue()
+    {
+        if (!hasBeenShown)
+        {
+            hasBeenShown = true;
+            FindObjectOfType<TutorialMessageManager>().StartDialogue(instruction);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered!");
+        TriggerDialogue();
+    }
+}
