6073e32fafd5f0a1ccdcd68d6ff65ffae515d94c
Orlando Estrella L <oestrellal96@gmail.com>
2018-05-22 14:20:21 -0700

    Added main menu and minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+
+    public Transform player;
+
+	void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = player.position.y + 20;
+        transform.position = newPosition;
+    }
+}
