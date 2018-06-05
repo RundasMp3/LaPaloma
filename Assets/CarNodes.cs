3eb862d568b4e7e7cf34e05a9ed57320bb0cd6f2
RundasMp3 <jorge.mendez@cetys.edu.mx>
2018-06-05 10:58:52 -0700

    Car filling
    Cars in the big city

---
+++ Assets/CarNodes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarNodes : MonoBehaviour {
+
+    public GameObject Frente;
+    public GameObject Derecha;
+    public GameObject Izquierda;
+    public GameObject DerechaDos;
+    public GameObject IzquierdaDos;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        if (Frente != null)
+            Gizmos.DrawLine(transform.position, Frente.transform.position);
+        if (Derecha != null)
+            Gizmos.DrawLine(transform.position, Derecha.transform.position);
+        if (Izquierda != null)
+            Gizmos.DrawLine(transform.position, Izquierda.transform.position);
+        if (DerechaDos != null)
+            Gizmos.DrawLine(transform.position, DerechaDos.transform.position);
+        if (IzquierdaDos != null)
+            Gizmos.DrawLine(transform.position, IzquierdaDos.transform.position);
+    }
+}
---
+++ Assets/CarScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour {
+
+    // Update is called once per frame
+    public GameObject node;
+    private Transform target;
+    private CarNodes nextNode;
+    private float speed = 50f;
+    private List<GameObject> nodeList;
+    int num;
+
+    private void Start()
+    {
+        target = node.transform;
+        nodeList = new List<GameObject>();
+    }
+
+    void FixedUpdate () {
+        Drive();
+	}
+
+    private void Drive()
+    {
+        float step = speed * Time.deltaTime;
+        System.Random rnd = new System.Random();
+
+        if (Vector3.Distance(transform.position, target.position) < 3f)
+        {
+            nextNode = node.GetComponent<CarNodes>();
+
+
+            if (nextNode.Izquierda != null)
+                nodeList.Add(nextNode.Izquierda);
+
+            if (nextNode.Frente != null)
+                nodeList.Add(nextNode.Frente);
+
+            if (nextNode.Derecha != null)
+                nodeList.Add(nextNode.Derecha);
+
+            if (nextNode.DerechaDos != null)
+                nodeList.Add(nextNode.DerechaDos);
+
+            if (nextNode.IzquierdaDos != null)
+                nodeList.Add(nextNode.IzquierdaDos);
+
+            //num = rnd.Next(0, nodeList.Count-1);
+            num = rnd.Next(nodeList.Count);
+            target = nodeList[num].transform;
+
+            node = nodeList[num];
+
+            nodeList.Clear();
+
+
+        }
+        else
+        {
+            Vector3 targetDir = target.position - transform.position;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            transform.rotation = Quaternion.LookRotation(newDir);
+            Debug.Log(name+" en "+Vector3.Distance(transform.position, target.position));
+        }
+    }
+
+}
